c7ae0175e10de42fb9de25d3be2ee24bd01af5f8
Anastasiia <blacsharm@gmail.com>
2024-03-25 23:13:12 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Discription from './Discription/Discription'
+import Options from './Options/Options'
+
+function App() {
+  return (
+    <div>
+    <Discription />
+      <Options />
+    </div>
+  )
+}
+
+export default App
